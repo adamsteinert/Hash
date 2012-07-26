ec0f57e74e5c5111ddf8dca52b6e96bc63f3a0f4
Adam Steinert <adam.j.steinert@gmail.com>
2012-07-26 10:29:41 -0500

    Add project summary for hours.
    Right now this is just a cheesy alert and an extra window.  Need to
    refactor this junk and make it pretty.

--- TargetProcess/tp_group_projects.user.js
+++ TargetProcess/tp_group_projects.user.js
@@ -9,40 +9,94 @@
 // ==/UserScript==
 
 var colors = ['#907D6D', '#7D2A2A', '#415757', '#9bcc9b', '#4e7373', '#0F0F0F'];
-var projects = function() {};
+var projects = new Array();
+var projectTotals = new Array();
 
 function highlight()
 {
-    projects = function() {};
+    projects = new Array();
+    projectTotals = new Array();
 	var i = 0;
+
+    
 	$('.dataRow').each(function(index, ele) {
 		var name = $('td:first-child', ele).text().trim();
-		
+	
+        var idRegexp = /\?[a-zA-Z]+ID=?(\d+)/g;
+        var link = $('td:nth-child(3)', ele);
+        var fst = $('a', link).first();
+        var match = idRegexp.exec(fst.attr('href'));
+        
 		if(!projects[name])
 		{
 			projects[name] = colors[i];
 			i++;
+            
+            //prep the totals object
+            projectTotals[name] = 0;
 		}
+        
+        var sum = getSumForProjectRow(ele);
+        projectTotals[name] += sum;
 		
-		//alert($('td:first-child', ele).text());
-		//$('td', ele).css('background', projects[name]);
         $('td:first-child', ele).css('background', projects[name]);
+        $('td:first-child', ele).append('(' + match[1] + ')');
 	});
 }
 
+function alertProjectTotals() {
+    var message = "Project Totals:\r\n";   
+    var total = getAllHours();
+    
+    for(var propt in projectTotals){
+        var hrs = projectTotals[propt];
+        var pct = (hrs / total) * 100;       
+        message += propt + ": " + hrs + "     (" + pct.toFixed(2) + "%)\r\n";
+    }
+    
+    alert(message);
+}
+
+function getAllHours() {
+    var sum = 0;
+    for(var propt in projectTotals){
+        sum += projectTotals[propt];
+    }
+    return sum;
+}
+
+function getSumForProjectRow(ele) {
+    var sum = 0;
+    $('.timeButton', ele).each(function() {
+        sum += Number($(this).val());
+    });    
+
+    return sum;
+}
+
+
 function insertUi()
 {
-    $('<p class="tptshhighlight">Refresh Highlights</p>').insertAfter('.settings');
+    $('<p class="tptshhighlight">Refresh highlights</p>').insertAfter('.settings');
     $('.tptshhighlight').css('cursor', 'pointer');
     $('.tptshhighlight').click(function () {
         highlight();
     });
+    
+    
+    $('<p class="tptshalert">Show Totals</p>').insertAfter('.settings');
+    $('.tptshalert').css('cursor', 'pointer');
+    $('.tptshalert').click(function () {
+        alertProjectTotals();
+    });
 }
+
+
 
 function initialize()
 {
     insertUi();
-    highlight();
+    highlight();    
 }
 
 console.log('starting up...');
